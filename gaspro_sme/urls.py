6d5fd122d975022dcf8f5aeb5afb195aaf7ec24b
Dpsalmist <sdamilare420@gmail.com>
2023-05-12 14:51:55 +0100

    reinstalled requirements.tx

--- gaspro_sme/urls.py
+++ gaspro_sme/urls.py
@@ -24,6 +24,7 @@ from django.views.generic import TemplateView
 # from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+
     path("admin/", admin.site.urls),
     path('api/v2/accounts/', include('accounts.urls')),
     path('api/v2/assets/', include('asset.urls')),
@@ -38,12 +39,7 @@ urlpatterns = [
     path('api/v2/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v2/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
-    # fcm template
-    # path("firebase-messaging-sw.js",
-    #     TemplateView.as_view(template_name="firebase-messaging-sw.js",content_type="application/javascript",
-    #     ),name="firebase-messaging-sw.js"
-    # ),
-
+    
     #path('swagger-docs/', schema_view),
     #path('docs/', include_docs_urls(title='HFV2 APIs')),
     #path('schema/', schema_view),
