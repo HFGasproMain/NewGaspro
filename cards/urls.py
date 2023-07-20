f4f9dce1f4aa6ef15ad89465c6ede189b0c2f332
HF GasProMain <134396221+HFGasproMain@users.noreply.github.com>
2023-07-20 17:59:50 +0100

    fixed bugs on urls.py

--- cards/urls.py
+++ cards/urls.py
@@ -3,7 +3,7 @@ from .views import CardCreateView, CardListView, CardDetailView, UserCardDetailV
 
 urlpatterns = [
     path('add-card/', CardCreateView.as_view(), name='card-create'),
-    path('list/', CardListView.as_view(), name='card-list'),
+    #path('list/', CardListView.as_view(), name='card-list'),
     path('cards/<int:pk>/', CardDetailView.as_view(), name='card-detail'),
     path('user/<int:user_id>/', UserCardDetailView.as_view(), name='card-list-by-user-id'),
     path('user-cards/<int:user_id>/', CardListView.as_view(), name='card-list'),
