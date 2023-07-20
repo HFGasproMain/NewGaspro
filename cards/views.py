75557a0ab7a502d35b2e3afc602eea243f6763b1
HF GasProMain <134396221+HFGasproMain@users.noreply.github.com>
2023-07-20 17:50:43 +0100

    implemented cards list by user_id

--- cards/views.py
+++ cards/views.py
@@ -9,9 +9,9 @@ class CardCreateView(generics.CreateAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
 
-class CardListView(generics.ListAPIView):
-    queryset = Card.objects.all()
-    serializer_class = CardSerializer
+# class CardListView(generics.ListAPIView):
+#     queryset = Card.objects.all()
+#     serializer_class = CardSerializer
 
 
 class UserCardDetailView(generics.RetrieveAPIView):
@@ -22,16 +22,17 @@ class UserCardDetailView(generics.RetrieveAPIView):
         queryset = Card.objects.all()
         user_id = self.request.query_params.get('user_id')
         #user_first_name = self.request.query_params.get('user_first_name')
-
         if user_id:
             queryset = queryset.filter(user_id=user_id)
-
-        # if user_first_name:
-        #     users = User.objects.filter(first_name__icontains=user_first_name)
-        #     queryset = queryset.filter(user__in=users)
 
         return queryset
 
 class CardDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
+class CardListView(generics.ListAPIView):
+    serializer_class = CardSerializer
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Card.objects.filter(user_id=user_id)
